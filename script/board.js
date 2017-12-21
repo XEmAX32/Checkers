c1a8cc5ca41c2125b9f4736d7d8115a4d6921c0d
Emanuele Sacco <XEmAX32@gmail.com>
2017-12-21 11:47:29 +0100

    Create board.js

---
+++ script/board.js
@@ -0,0 +1,12 @@
+var board = document.getElementById("chess-board"),
+    ctx = board.getContext("2d");
+
+for (var n = 0; n < 4; n++) {
+    for (var m = 0; m < 4; m++) {
+        ctx.beginPath();
+        ctx.rect(n * 75, m * 75, 37.5, 37.5);
+        ctx.rect((2 * n + 1) * 37.5, (2 * m + 1) * 37.5, 37.5, 37.5);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
